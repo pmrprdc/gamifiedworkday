cc3371cdf4d51893a8256a1d79c308b4bce592c6
Pamir Perdeci <pamirperdeci@gmail.com>
2024-04-02 10:31:05 -0400

    added sales page sections

--- src/components/SalesPage.jsx
+++ src/components/SalesPage.jsx
@@ -4,7 +4,15 @@ function SalesPage() {
   return (
     <div>
       <h1>Sales Page</h1>
-      {/* Add any sales-specific content here */}
+        <h3>Bold Promise section</h3>
+        <h3>Pain Point section</h3>
+        <h3>Benefits section</h3>
+        <h3>The solition disovery section</h3>
+        <h3>Solution introduction</h3>
+        <h3>Solution breakdown</h3>
+        <h3>Social proof & Testimonials</h3>
+        <h3>Reminder of what's included</h3>
+        <h3>faq</h3>
     </div>
   );
 }
