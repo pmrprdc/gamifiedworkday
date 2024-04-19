e759f8d42b8475d1f514f1c493968735a27176e6
Pamir Perdeci <pamirperdeci@gmail.com>
2024-04-19 10:48:13 -0400

    sales page updated

--- src/components/SalesPage.jsx
+++ src/components/SalesPage.jsx
@@ -8,15 +8,31 @@ function SalesPage() {
             <p>Gamify Life & Enjoy Boring repetitive tasks</p>
             <p>Make Boring Tasks Fun Again</p>
             <p>Make Work Fun</p>
+            <p>Gradually increase work volume while avoiding fatigue</p>
+            <p>Gradually increase work speed and efficiency</p>
+            <p>track work consistency and adjust work frequency tips section</p>
         <h3>Pain Point section</h3>
-            <p>ADHD</p>
-            <p>Boring taks</p>
-            <p>Trouble engaging</p>
-            <p>Lack of improvement or lack of pride</p>
+            <p>Not being able to get enough work done</p>
+            <p>Struggle Starting Tasks</p>
+            <p>Trouble engaging with tasks too easy</p>
+            <p>Fail to implement habits on your own</p>
         <h3>Benefits section</h3>
-            <p>Make work fun</p>
+            <p>Get more work done</p>
+            <p>Get the same work done faster and faster over time</p>
+            <p>Get more enjoyment out of doing your work</p>
         <h3>The solition disovery section</h3>
-            <p>When I took up kinobody, I learned how to make terribly boring workouts exciting.</p>
+            <p>I had ambition, intelligent ideas, excellent at making plans but suffered greatly in execution</p>
+            <p>I knew that if I had a team under me I could potentially do great things but I couldn't even funtion enough to do any respectable output</p>
+            <p>I failed for years to execute things fast enough</p>
+            <p>Based on my workouts, i was realising I never did enough volume and intensity </p>
+            <p>I tried many routines yet I always felt that I could be doing more</p>
+            <p>It became more and more obvious that I had a terrible time with outputing work</p>
+            <p>I was diagnosed with ADHD however I could not handle how the adhd meds made me feel</p>
+            <p>I learned from healthy gamerGG what I was really doing</p>
+            <p>Explain GamerGG Adhd behavior</p>
+            <p>Then I recalled how I gamified my workouts and my body was really the only thing I could show for in results even though I'd been studying
+              business.
+            </p>
         <h3>Solution introduction</h3>
             <p>Once I realised gamifying my workouts work I thought what if it works with by computer tasks.</p>
             <p>I wanted to race against myself regardless. Sommething about it makes me engage and having intentions makes a difference.</p>
