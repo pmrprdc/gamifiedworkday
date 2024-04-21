5d73073bbf3219003e81a0fcb3cce785efc19c2b
Pamir Perdeci <pamirperdeci@gmail.com>
2024-04-21 11:04:06 -0400

    created a stopwatch component

--- src/components/PrivatePage.jsx
+++ src/components/PrivatePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Timer from './Timer';
 import SetupInputs from './setupInputs'
+import StopWatch from './StopWatch'
 
 function PrivatePage() {
   return (
     <div>
       <h1>Private Page</h1>
       <SetupInputs />
+      <StopWatch />
       <Timer initialMinutes={2} initialSeconds={30} />
       <Timer initialMinutes={2} initialSeconds={30} />
       <Timer initialMinutes={2} initialSeconds={30} />
---
+++ src/components/StopWatch.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && !isPaused) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);  // increment the time by 10 milliseconds
+      }, 10);
+    } else if (!isActive && time !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const handleStop = () => {
+    setIsPaused(true);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  const formatTime = () => {
+    const getMilliseconds = `0${(time % 1000) / 10}`.slice(-2);
+    const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2);
+    return `${seconds}:${getMilliseconds}`;
+  };
+
+  return (
+    <div>
+      <h2>Timer</h2>
+      <div id="display-timer">{formatTime()}</div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+
+export default Timer;
