48ade4edbc6047c5a66115c573800846d071ae32
Pamir Perdeci <pamirperdeci@gmail.com>
2024-05-21 21:59:28 -0400

    fixed opacity

--- src/components/PRIVATE/PrivatePage.jsx
+++ src/components/PRIVATE/PrivatePage.jsx
@@ -8,7 +8,7 @@ const PrivatePage = () => {
     <MatrixEffect style={{zIndex: '1', marginTop: '0'}}/>
     <div style={{ position: 'relative', width: '100%', height: '100vh', opacity:'80%', zIndex: "1"}}>  
      
-      <div style={{ border: '1px solid blue', marginTop: '20px',position: 'relative', opacity: "40%", zIndex: "0" }}>  
+      <div style={{ border: '1px solid blue', marginTop: '20px',position: 'relative', opacity: "100%", zIndex: "0" }}>  
         <h1>Private Page</h1>
         <VisualTimer />
       </div>
