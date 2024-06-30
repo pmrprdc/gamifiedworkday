76bd1c2fbf9043f077f543c5e1fac10dbd0dce79
Pamir Perdeci <pamirperdeci@gmail.com>
2024-06-30 17:54:18 -0400

    simplified app further by removing private page matrix effect

--- src/components/PRIVATE/PrivatePage.jsx
+++ src/components/PRIVATE/PrivatePage.jsx
@@ -5,14 +5,14 @@ import MatrixEffect from '../MatrixEffect'; // Adjust the path as necessary if i
 const PrivatePage = () => {
   return (
     <>
-    <MatrixEffect style={{zIndex: '1', marginTop: '0'}}/>
-    <div style={{ position: 'relative', width: '100%', height: '100vh', opacity:'80%', zIndex: "1"}}>  
+
+   
      
-      <div style={{ border: '1px solid blue', marginTop: '20px',position: 'relative', opacity: "100%", zIndex: "0" }}>  
+     
         <h1>Private Page</h1>
         <VisualTimer />
-      </div>
-    </div>
+     
+  
     </>
   );
 
